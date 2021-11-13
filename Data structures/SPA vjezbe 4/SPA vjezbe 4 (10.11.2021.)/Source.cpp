c70939a920439608fa517238c6f23f874106fa1c
Haris Kordic <kordicharis18@gmail.com>
2021-11-13 15:24:23 +0100

    Update Source.cpp

--- Data structures/SPA vjezbe 4/SPA vjezbe 4 (10.11.2021.)/Source.cpp
+++ Data structures/SPA vjezbe 4/SPA vjezbe 4 (10.11.2021.)/Source.cpp
@@ -16,8 +16,9 @@ void Testiranje(PrioritetniQueue& R)
 	srand(time(0));//Ceed-amo rand kako ne bi imali slicne vrijednosti;
 	for (int i = 0; i < 20; i++)
 	{
-	   cout<<"Dodavanje elementa "<<GetRandomBroj()<<" u niz"<<endl;
-	   R.Dodaj(GetRandomBroj());
+	   int x=GetRandomBroj();
+	   cout<<"Dodavanje elementa "<<x<<" u niz"<<endl;
+	   R.Dodaj(x);
 	}
 
 	for (int i = 0; i < 21; i++)
